8e590d48ee309f245be5db9a6c96772159ae96e1
skueee <skueee@outlook.com>
2026-09-05 00:34:46 +0200

    Fixed next config

--- frontend/noomo-frontend/next.config.ts
+++ frontend/noomo-frontend/next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  module.exports = {
-    allowedDevOrigins: ['noomo.skue.hackclub.app'],
-  }
+  allowedDevOrigins: ['noomo.skue.hackclub.app'],
   reactCompiler: true,
 };
 
