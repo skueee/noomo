da2d40cf8f834caf95073c51a3e69ba82910c409
skueee <skueee@outlook.com>
2026-09-05 13:23:29 +0200

    Better layout (probably)

--- frontend/noomo-frontend/src/app/page.tsx
+++ frontend/noomo-frontend/src/app/page.tsx
@@ -6,10 +6,12 @@ import React from 'react';
 export default function Home() {
   return (
     <main className="min-h-screen w-full flex items-center justify-center p-4">
-      <div className="flex flex-col items-center gap-6 max-w-md w-full p-8">
+      <div className="flex flex-col items-center gap-5 max-w-md w-full p-0">
         <h1 className="kalnia-title">Noomo</h1>
-        <div className="play-rect w-[520px] h-[92px]" style={{ boxShadow: "inset 0px -6px 0px 0px #251d09" }} />
-        <div className="challenge-rect w-[500px] h-[54px]"/>
+        <div className="flex flex-col items-center gap-3 max-w-md w-full p-0">
+          <div className="play-rect w-[520px] h-[92px]" style={{ boxShadow: "inset 0px -6px 0px 0px #251d09" }} />
+          <div className="challenge-rect w-[500px] h-[54px]" />
+        </div>
       </div>
     </main>
   );
