28ebd07fa93ec7e2808148f83cdad9e360da9e2f
skueee <skueee@outlook.com>
2026-09-06 20:36:18 +0200

    Improved the words selections by lowering some probabilities and filtering characters

--- backend/src/noomo_backend/model_script.py
+++ backend/src/noomo_backend/model_script.py
@@ -3,6 +3,7 @@
 
 import gc
 import os
+import regex as re
 from pathlib import Path
 
 import torch
@@ -27,17 +28,52 @@ def predict(model, tokenizer, text, words_count):
     next_token_logits = outputs.logits[
         0, -1, :
     ]
+
+    bad_words_ids = [
+        tokenizer.eos_token_id,
+        tokenizer.pad_token_id,
+        tokenizer.unk_token_id,
+        tokenizer.sep_token_id,
+        tokenizer.encode("\n", add_special_tokens=False)[0]
+    ]
+
+    for bad_id in bad_words_ids:
+        if bad_id is not None:
+            next_token_logits[bad_id] = float('-inf')
+
     probabilities = torch.softmax(next_token_logits, dim=-1)
 
-    top_probs, top_indices = torch.topk(probabilities, words_count)
+    top_probs, top_indices = torch.topk(probabilities, 50)
 
-    response = []
-    for i in range(words_count):
+    candidates = []
+    for i in range(50):
         token_text = tokenizer.decode([top_indices[i]])
         prob = top_probs[i].item() * 100
-        response.append({"word":token_text, "prob":prob})
+        candidates.append({"word":token_text, "prob":prob})
 
-    return response
+    response = []
+    words = []
+    for i in candidates:
+        choose = True
+
+        pattern = re.compile(r"\s[A-Za-z0-9]+", re.IGNORECASE)
+        if pattern.match(i["word"]):
+            curr_word = i["word"][1:]
+        else:
+            curr_word = i["word"]
+
+        if curr_word in words:
+            choose = False
+
+        pattern = re.compile(r"^\p{Script=Latin}+$")
+        if not pattern.match(curr_word):
+            choose = False
+
+        if choose:
+            response.append({"word":curr_word, "prob":i["prob"]})
+            words.append(curr_word)
+
+    return response[:words_count]
 
 def clear_model(model, tokenizer):
     del model
