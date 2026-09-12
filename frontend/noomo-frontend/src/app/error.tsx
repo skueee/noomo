9d2c1f5a52001478d4d6efefb1fb9f1ea722aaf8
skueee <skueee@outlook.com>
2026-09-12 17:25:15 +0200

    Escaping characters stuff

--- frontend/noomo-frontend/src/app/error.tsx
+++ frontend/noomo-frontend/src/app/error.tsx
@@ -6,7 +6,7 @@ const Error = ({ error }: { error: Error })=> {
     <div className="flex flex-col items-center justify-center w-full h-full">
       <a className="kalnia-title text-[32px]">Hum, something is not right ?</a>
       <a className="kalnia-main text-[28px]">Error : {error.message}</a>
-      <a className="kalnia-main text-[10px]">Don't worry, it's probably a feature and totally not a bug</a>
+      <a className="kalnia-main text-[10px]">Don&apos;t worry, it&apos;s probably a feature and totally not a bug</a>
     </div>
   );
 }
