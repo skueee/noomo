9b28011f2789919ea52bd040d4f3d485181e8f08
skueee <skueee@outlook.com>
2026-09-16 15:31:01 +0200

    Added get-challenges call

--- frontend/noomo-frontend/src/app/actions.tsx
+++ frontend/noomo-frontend/src/app/actions.tsx
@@ -5,13 +5,16 @@
 
 "use server";
 
+// Change this if you run the backend locally
+const backend = "https://api.skue.hackclub.app/";
+
 // Call to the backend to get the words for the game
 export async function getPredictions(input: string) {
   const isProduction =
     typeof process.env.NEXT_PUBLIC_VERCEL_ENV !== "undefined" &&
     process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
 
-  const response = await fetch("https://api.skue.hackclub.app/predict", {
+  const response = await fetch(backend + "/predict", {
     cache: "no-store",
     method: "POST",
     headers: { "Content-Type": "application/json", Accept: "application/json" },
@@ -20,6 +23,16 @@ export async function getPredictions(input: string) {
       sentence: input,
       prod: isProduction,
     }),
+  });
+  const json = await response.json();
+  return json;
+}
+
+// Call to get today's challenge
+export async function getChallenge(input: string) {
+  const response = await fetch(backend + "/daily-challenge", {
+    cache: "no-store",
+    method: "GET",
   });
   const json = await response.json();
   return json;
